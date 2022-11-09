9149969a0638ed033f5e77bfc46c11bfdf0506c5
NorenaCarlos <70341407+NorenaCarlos@users.noreply.github.com>
2022-11-08 19:32:31 -0500

    some modifications on json, to deploy

--- Asincronismo JavaScript/srcProject/main.js
+++ Asincronismo JavaScript/srcProject/main.js
@@ -1,5 +1,5 @@
 const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UC8LeXCWOalN8SxlrPcG-PaQ&part=snippet%2Cid&order=date&maxResults=9';
-const content = document.getElementById("content");
+const content = null || document.getElementById("content");
 
 const options = {
 	method: 'GET',
@@ -18,6 +18,7 @@ async function fetchData(urlAPI){
 (async () => {
     try{
         const videos = await fetchData(API);
+        console.log(videos);
         let view = `
         ${videos.items.map(video => `<div class="group relative">
         <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
@@ -34,17 +35,4 @@ async function fetchData(urlAPI){
     } catch(error) {
         console.log(error);
     }
-})()
-/*
-<div class="group relative">
-    <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-        <img src="" alt="" class="w-full">
-    </div>
-    <div class="mt-4 flex justify-between">
-        <h3 class="text-sm text-gray-700">
-        <span aria-hidden="true" class="absolute inset-0"></span>
-        title
-        </h3>
-    </div>
-</div>
-*/
+})();
