39b5e7c7e6b677b48c70699ee96ba066fe50dd3a
NorenaCarlos <carlos1997704@gmail.com>
2022-11-29 00:39:00 -0500

    python

--- python/web-server/main.py
+++ python/web-server/main.py
@@ -1,4 +1,17 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+  return[1,2,3]
+
+
+@app.get('/contact')
+def get_list():
+  return {'name': "Platzi"}
 
 def run():
   store.get_categories()
--- python/web-server/store.py
+++ python/web-server/store.py
@@ -1,17 +1,4 @@
 import requests
-from fastapi import FastAPI
-
-
-app = FastAPI()
-
-@app.get('/')
-def get_list():
-  return[1,2,3]
-
-
-@app.get('/contact')
-def get_list():
-  return {name: "Platzi"}
 
 
 
