e581f7e494e2adfcfab1c6ad589d7a6834ff5eb7
NorenaCarlos <carlos1997704@gmail.com>
2022-12-14 22:13:55 -0500

    clases gulp y otros

--- nodeJsBackend/herramientas/scraping/index.js
+++ nodeJsBackend/herramientas/scraping/index.js
@@ -3,10 +3,11 @@ const puppeteer = require('puppeteer');
 (async ()=>{
     console.log('Lanzamos el navegador');
     // const browser = await puppeteer.launch();
-    const browser = await puppeteer.launch({headless:false});
+    // const browser = await puppeteer.launch({headless:true});
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://es.wikipedia.org/wiki/Node.js');
-
+    //await page.goto('https://es.wikipedia.org/wiki/Node.js');
+    await page.goto('https://www.google.com');
     console.log('Cerramos navegador ....');
     browser.close();
     console.log('Navegador cerrado');
