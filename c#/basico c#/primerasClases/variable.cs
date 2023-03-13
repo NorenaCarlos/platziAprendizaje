8e7303b95fbf0576fdda3f71854b0eec9163a3c8
NorenaCarlos <70341407+NorenaCarlos@users.noreply.github.com>
2023-03-13 10:54:33 -0500

    C#

---
+++ c#/basico c#/primerasClases/variable.cs
@@ -0,0 +1,19 @@
+/*
+int a = 2000000; //las variables int guardan de -2147483648 hasta 2147483647
+bool b = true; // booleanos representan 1 bit su estado puede ser true o false
+float pi = 3,141592; //almacena valores de coma flotante de hasta 6 o 7 digitos
+double PI = 3.1415926535; // almacena valores de coma flotante de hasta 15 digitos
+char letra = "a"; // este tipo de variable guarda un solo simbolo en 2 bytes
+string frase = "La sabiduria empezó con un duda"; // este tipo de variavle almacena una cadera de caracteres o simbolos
+long numeroGrande = 1654674964964; //almacena valores entre -9223372036854775808 hasta 9223372036854775807
+uint entero = 156431; // almacena valores entre 0 y 4294967295
+//Esos son todos los tipos de variable o datos primitivos de este lenguaje
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(pi);
+Console.WriteLine(PI);
+Console.WriteLine(letra);
+Console.WriteLine(frase);
+Console.WriteLine(numeroGrande);
+Console.WriteLine(entero);
+*/
